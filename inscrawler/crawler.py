3f4031f2516417a1d57399961d4b00422eec25a8
Flávio <flaviowildner@hotmail.com>
2020-07-28 22:21:08 -0300

    removed login when instantiating inscrawler

--- inscrawler/crawler.py
+++ inscrawler/crawler.py
@@ -71,8 +71,7 @@ class InsCrawler(Logging):
     def __init__(self, has_screen=False):
         super(InsCrawler, self).__init__()
         self.browser = Browser(has_screen)
-        self.page_height = 0
-        self.login()
+        self.page_height = 0        
 
     def _dismiss_login_prompt(self):
         ele_login = self.browser.find_one(".Ls00D .Szr5J")
